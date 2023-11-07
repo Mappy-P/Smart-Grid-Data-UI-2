94cd067d412a9867c4bb23665f9d06935a4eb709
mappy <mathijs.andries@gmail.com>
2023-11-07 14:04:48 +0100

    Zelfde commit als vorige keer, maar toen werkte hij niet. Nu hopelijk wel

---
+++ website/static/index.js
@@ -0,0 +1,29 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } 
+      // else {
+      //   entry.target.classList.remove('show');
+      // }
+  });
+});
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show2');
+      } 
+      // else {
+      //   entry.target.classList.remove('show');
+      // }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el) => observer2.observe(el));
