41946b533fcf2953688b73c51d222631e4ed445f
mappy <mathijs.andries@gmail.com>
2023-11-13 22:48:04 +0100

    Afwerking MLmodel class en incorporartie consumptie ML model met werkende functie

--- website/models.py
+++ website/models.py
@@ -1,10 +1,10 @@
-from MLmodel import DemoModel
+from .MLmodel import DemoModel
+
 
 linkconsumptie = (r'website\assets\model.h5')
 linkconsumptiedata = (r'website\assets\data\consumption_EnergyVille.csv')
 
-consumptieModel = DemoModel(linkconsumptie, linkconsumptiedata)
-
 def predictConsumptie(start, duration): #De duration is 0 als je voor 1 dag wilt voorspellen. x als je voor x extra dagen wilt voorspellen.
-    predictie, echteWaarden = DemoModel.predict(start, duration)
+    consumptieModel = DemoModel(linkconsumptie, linkconsumptiedata)
+    predictie, echteWaarden = consumptieModel.predictValues(start, duration)
     return predictie, echteWaarden
--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from . import models
+from datetime import datetime
 import optimisation
 
 views = Blueprint('views', __name__)
@@ -15,6 +17,18 @@ def about():
 def demo():
     if request.method == 'POST':
         typeOfCalculation = request.form.get('typeOfCalculation')
-        startDate = request.form.get('startDate')
-        endDate = request.form.get('endDate')
-    return render_template('demo.html')
+        startDate = datetime.strptime(request.form.get('startDate'), '%Y-%m-%d')
+        endDate = datetime.strptime(request.form.get('endDate'), '%Y-%m-%d')
+
+        if startDate > endDate:
+            flash("Your end-date cannot be before your start-date", category='error')
+
+        else:
+            duration = (endDate - startDate).days
+            beginDateData = datetime.strptime('2018-01-11', '%Y-%m-%d')
+            start = (startDate - beginDateData).days
+            predictie = models.predictConsumptie(start, duration)
+    else:
+        predictie = None
+
+    return render_template('demo.html', result = predictie)
